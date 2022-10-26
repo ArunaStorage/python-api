cb3d110fbf795a8e2c2930c8a373510d130bb0ac
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2022-10-26 10:20:46 +0200

    Fix setup.cfg and include missing .pyi files in package build

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     url='https://github.com/ArunaStorage/python-api',
     license='Apache 2.0',
     author='Marius Dieckmann, Jannis Hochmuth',
-    author_email='marius.dieckmann@computational.bio.uni-giessen.de, jannis.hochmuth@computational.bio.uni-giessen.de',
+    author_email='marius.dieckmann@computational.bio.uni-giessen.de, '
+                 'jannis.hochmuth@computational.bio.uni-giessen.de',
     # packages=find_packages('aruna')
     packages=['aruna',
               'aruna.api',
@@ -20,8 +21,12 @@ setup(
               'aruna.api.storage',
               'aruna.api.storage.models', 'aruna.api.storage.models.v1',
               'aruna.api.storage.services', 'aruna.api.storage.services.v1'],
+    package_data={'aruna.api.internal.v1': ['*.pyi'],
+                  'aruna.api.notification.services.v1': ['*.pyi'],
+                  'aruna.api.storage.models.v1': ['*.pyi'],
+                  'aruna.api.storage.services.v1': ['*.pyi']},
     install_requires=[
-        'protobuf>=3.20.0',
+        'protobuf<=3.20.0',
         'grpcio',
         'grpc-gateway-protoc-gen-openapiv2',
         'google-api-python-client'
